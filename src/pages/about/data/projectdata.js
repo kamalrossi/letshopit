60d09c9be42bbff8944499a0f73fd149a1fe0d2d
kamalrossi <rossikamal@gmail.com>
2022-06-30 17:53:24 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,50 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "letshopit";
+
+const projectdesc = {
+  title: "Title",
+  desc:
+    "letshopit is a responsive web app for shopping experience in an  online marketplace.Major modules include  classfied product, cart, recommended product, contact, etc."
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag3: "JSX",
+    tag4: "E-Commerce"
+  }
+];
+
+const projecttime = [
+  {
+    title: "Design",
+    from: "Dec 21",
+    to: "Feb 22"
+  },
+  {
+    title: "Implementation",
+    from: "Dec 21",
+    to: "Feb 22"
+  }
+];
+
+const projectrole = [
+  {
+    title: "UI & UX Design",
+    desc:
+      "UX development Requirement analyze and preparation of mock prototype of e-commerce platform"
+  },
+  {
+    title: "Responsive Web development",
+    desc:
+      "Coding and implementation Implement required modules of ecommerce apps such as cart, payment, product card etc."
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
